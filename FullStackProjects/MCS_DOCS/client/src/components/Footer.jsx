6b29ecab1b630849cf23757d7c96e15c213aa769
Raghav Awasthi <raghavawasthi240@gmail.com>
2023-10-16 22:15:09 +0530

    search functionality added

--- FullStackProjects/MCS_DOCS/client/src/components/Footer.jsx
+++ FullStackProjects/MCS_DOCS/client/src/components/Footer.jsx
@@ -1,49 +1,62 @@
 import React from 'react'
 import Logo from "../assets/logoWhite.png"
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 
 const Footer = () => {
   return (
     <div>
         <div className='bg-gradient-to-r from-[#9400D3] to-[#0076CE] text-white'>
-            <div className='flex justify-between w-[90%] mx-auto p-4'>
+            <div className='flex justify-between w-[90%] mx-auto p-4 gap-4 md:flex-row flex-col '>
                 <div>
                     <img src={Logo} alt="" />
                     <p>India's first platform dedicated to simplifying partner search</p>
                 </div>
-                <ul className='flex flex-col gap-2'>
-                    <li className='font-bold'>COMPANY</li>
-                    <li>About</li>
-                    <li>Pricing</li>
-                    <li>Careers</li>
-                </ul>
-                <ul className='flex flex-col gap-2'>
-                    <li className='font-bold'>SOLUTIONS</li>
-                    <li>Search</li>
-                    <li>Connect</li>
-                    <li>Research</li>
-                    <li>Academy</li>
-                </ul>
-                <ul className='flex flex-col gap-2'>
-                    <li className='font-bold'>RESOURCES</li>
-                    <li>Blogs</li>
-                    <li>Forms</li>
-                </ul>
-                <ul className='flex flex-col gap-2'>
-                    <li className='font-bold'>SUPPORT</li>
-                    <li>Help</li>
-                    <li>Contact Us</li>
-                </ul>
-                <ul className='flex flex-col gap-2'>
-                    <li className='font-bold'>LEGAL</li>
-                    <li>Privacy</li>
-                    <li>Terms</li>
-                    <li>Accessibility</li>
-                </ul>
+                <div className=' flex-col xs:flex-row justify-between gap-6 lg:gap-10 flex-wrap md:flex-nowrap'>
+                    <ul className='flex flex-col gap-2'>
+                        <li className='font-bold'>COMPANY</li>
+                        <li>About</li>
+                        <li>Pricing</li>
+                        <li>Careers</li>
+                    </ul>
+                    <ul className='flex flex-col gap-2'>
+                        <li className='font-bold'>SOLUTIONS</li>
+                        <li>Search</li>
+                        <li>Connect</li>
+                        <li>Research</li>
+                        <li>Academy</li>
+                    </ul>
+                    <ul className='flex flex-col gap-2'>
+                        <li className='font-bold'>RESOURCES</li>
+                        <li>Blogs</li>
+                        <li>Forms</li>
+                    </ul>
+                    <ul className='flex flex-col gap-2'>
+                        <li className='font-bold'>SUPPORT</li>
+                        <li>Help</li>
+                        <li>Contact Us</li>
+                    </ul>
+                    <ul className='flex flex-col gap-2'>
+                        <li className='font-bold'>LEGAL</li>
+                        <li>Privacy</li>
+                        <li>Terms</li>
+                        <li>Accessibility</li>
+                    </ul>
+                </div>
             </div>
             <hr />
 
-            <div>
+            <div className='flex justify-between w-[90%] mx-auto p-4'>
                 <p>Sambhaji Nagar, No2, St. Antony Road, Chembur, Mumbai - 400071, Maharashtra</p>
+                <div className='flex gap-4'>
+                    <FacebookIcon/>
+                     <InstagramIcon/>
+                       <LinkedInIcon/>
+                       <WhatsAppIcon/>
+                       
+                </div>
             </div>
 
         </div>
--- FullStackProjects/MCS_DOCS/client/src/pages/Home/components/LandingPage.jsx
+++ FullStackProjects/MCS_DOCS/client/src/pages/Home/components/LandingPage.jsx
@@ -1,68 +1,99 @@
-import { Button, OutlinedInput, TextField } from '@mui/material'
-import React, { useEffect, useState } from 'react'
-import LeftImg from "../../../assets/Left.png"
+import { Button, OutlinedInput, TextField } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import LeftImg from "../../../assets/Left.png";
 import BackgroungImg from "../../../assets/bg.png";
 import CuttingVector from "../../../assets/Vector.png";
-import { useDispatch, useSelector } from 'react-redux';
-import { currentCAFetch, fetchingCAList, searchingCA } from '../../../store/slices/personSlice';
-import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from "react-redux";
+import {
+  currentCAFetch,
+  fetchingCAList,
+  searchingCA,
+} from "../../../store/slices/personSlice";
+import { useNavigate } from "react-router-dom";
 
 const LandingPage = () => {
-  const [CAname,setCAname]=useState("");
-  const dispatch=useDispatch()
-  const [hide,setHide]=useState(false);
-  const navigate=useNavigate()
+  const [CAname, setCAname] = useState("");
+  const dispatch = useDispatch();
+  const [hide, setHide] = useState(false);
+  const navigate = useNavigate();
   // const [CAList,setCAList]=useState();
-  const CAList= useSelector(state=>state.person.variedCAList);
-  const search=(e)=>{
-    console.log("ghvvhg")
+  const CAList = useSelector((state) => state.person.variedCAList);
+  const search = (e) => {
+    console.log("ghvvhg");
     // dispatch(fetchingCAList(e.target.value))
-    dispatch(searchingCA(CAname))
-  }
+    dispatch(searchingCA(CAname));
+  };
 
- const getDetails=(item)=>{
-  dispatch(currentCAFetch(item))
-  navigate("/details")
- }
-  const fetchCAList=(e)=>{
-    dispatch(fetchingCAList(e.target.value))
+  const getDetails = (item) => {
+    dispatch(currentCAFetch(item));
+    navigate("/details");
+  };
+  const fetchCAList = (e) => {
+    dispatch(fetchingCAList(e.target.value));
     // setCAList(useSelector(state=>state.person.variedCAList))
     // console.log(CAList)
     // setCAname(e.target.value)
-  }
+  };
   return (
-    <div className='md:h-screen overflow-hidden flex flex-col md:flex-row'>
+    <div className="md:h-screen overflow-hidden flex flex-col md:flex-row">
       <div>
-        <img src={BackgroungImg} className='absolute w-screen h-screen overflow-hidden top-0 -z-10'/>
-        <img src={CuttingVector} className='w-screen overflow-hidden absolute bottom-0 -z-10'/>
+        <img
+          src={BackgroungImg}
+          className="absolute w-screen h-screen overflow-hidden top-0 -z-10"
+        />
+        <img
+          src={CuttingVector}
+          className="w-screen overflow-hidden absolute bottom-0 -z-10"
+        />
       </div>
-        <div className='w-full md:w-[60%] flex flex-col justify-center px-6 gap-6 lg:gap-10 mt-14 p-6'>
-          <p className='text-[2rem] sm:text-[3rem] lg:text-[4rem] font-bold leading-tight'>Find  Partners (CAs) available online</p>
-           <p className='text-gray-500'><span className='font-bold'>CONNECT</span> with us where your services are listed and visible to a myriad of businesses seeking CA’s for compliance support</p>
-           <div className='w-full '>
-             <div className='flex items-center'>
-               <OutlinedInput placeholder='Search by name' sx={{ width:"20rem",height:"3rem", borderRadius:"0", border:"none", outline:"0"}} onChange={fetchCAList}/>
-               <Button variant="contained" sx={{width:"10rem",height:"3rem", borderRadius:"0"}} onClick={search}>Search</Button>
-             </div>
-             <div className={`${hide?"hidden":"w-full bg-white"}`}>
-              {
-                CAList.map((item)=>{
-                  console.log(CAList)
-                  return(
-                    <p className='p-2 border-b' onClick={()=>getDetails(item)}>{item.name}</p>
-                  )
-                })
-              }
-              {/* <p className='p-2 border-b'>{CAList[0]?.name}</p> */}
-             
-             </div>
-           </div>
+      <div className="w-full md:w-[60%] flex flex-col justify-center px-6 gap-6 lg:gap-10 mt-14 p-6">
+        <p className="text-[2rem] sm:text-[3rem] lg:text-[4rem] font-bold leading-tight">
+          Find Partners (CAs) available online
+        </p>
+        <p className="text-gray-500">
+          <span className="font-bold">CONNECT</span> with us where your services
+          are listed and visible to a myriad of businesses seeking CA’s for
+          compliance support
+        </p>
+        <div className="w-full relative">
+          <div className="flex items-center">
+            <OutlinedInput
+              placeholder="Search by name"
+              sx={{
+                width: "20rem",
+                height: "3rem",
+                borderRadius: "0",
+                border: "none",
+                outline: "0",
+              }}
+              onChange={fetchCAList}
+            />
+            <Button
+              variant="contained"
+              sx={{ width: "10rem", height: "3rem", borderRadius: "0" }}
+              onClick={search}
+            >
+              Search
+            </Button>
+          </div>
+          <div className={`${hide ? "hidden" : "absolute top-[3rem] w-full bg-white z-40"}`}>
+            {CAList?.map((item) => {
+              console.log(CAList);
+              return (
+                <p className="p-2 border-b" onClick={() => getDetails(item)}>
+                  {item.name}
+                </p>
+              );
+            })}
+            {/* <p className='p-2 border-b'>{CAList[0]?.name}</p> */}
+          </div>
         </div>
-        <div className='flex justify-center items-center p-4'>
-          <img src={LeftImg}/>
-        </div>
+      </div>
+      <div className="flex justify-center items-center p-4">
+        <img src={LeftImg} />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default LandingPage
+export default LandingPage;
